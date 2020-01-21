9f6fb690fb37342d3015dca36c750b61dcf356fc
Atul kumar <atulkumar8775@gmail.com>
2020-01-21 19:54:54 +0530

    webpage updated

---
+++ webpage.php
@@ -0,0 +1,550 @@
+<!DOCTYPE html>
+<html> 
+<head>
+	<title>Travelling Agency</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous" />
+
+
+	<link rel="stylesheet" type="text/css" href="webpage.css">
+
+
+</head>
+
+<body>
+	
+	<div class="header" id='topheader'>
+	 <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container text-uppercase p-2">
+
+        <a class="navbar-brand font-weight-bold text-white" href="#">Atul travels</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto text-uppercase">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+
+          <li class="nav-item">
+             <a class="nav-link" href="#">Price</a>
+           </li>
+
+          <li class="nav-item">
+             <a class="nav-link" href="#">abouut</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section class="header-section">
+    <div class="center-div">
+      <h1 class="font-weight-bold">we are The Web Developer</h1>
+      <p>We create the world best website.</p>
+      
+      <div class="header-buttons">
+        <a href="#">AboutUs</a>
+        <a href="#">Contact</a>
+      </div>
+     </div>
+    </section>
+  </div>
+<!---header part end  --->
+
+
+
+
+
+
+<!---three extra div start-->
+
+<section class="header-extradiv">
+  <div class="container">
+    <div class="row">
+      <div class='extra-div col-lg-4 col-md-4 col-12'>
+        <a href="#"><i class="fa-3x fa fa-desktop" aria-hidden="true"></i></a>
+        <h2>EASY TO USE</h2>
+        <p>web development is easy to learn and make a awesome webpage.</p>
+      </div>
+
+        <div class='extra-div col-lg-4 col-md-4 col-12'>
+        <a href="#"><i class="fa-3x fa fa-trophy" aria-hidden="true"></i></a>
+        <h2>AWESOME DESIGN</h2>
+        <p>web development is easy to learn and make a awesome webpage.</p>
+      </div>
+
+        <div class='extra-div col-lg-4 col-md-4 col-12'>
+        <a href="#"><i class="fa-3x fa fa-magic" aria-hidden="true"></i></a>
+        <h2>EASY TO customize</h2>
+        <p>web development is easy to learn and make a awesome webpage.</p>
+       
+       </div>
+      
+    </div>
+    
+  </div>
+
+</section>
+
+<!---end three extra div start-->
+
+
+
+
+
+
+<!--offer section start-->
+<section class="Serviceoffers">
+  <div class="container heading text-center">
+    <h1 class="text-center font-weight-bold">WHAT DO WE OFFER</h1>
+    <p class="text-center">fast web design and database connection</p>  
+  </div>
+
+  <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-12 col-10 offset-1 offset-lg-0">
+          <div class="name my-3">
+            <h1>HTML</h1>
+            <div class="progress w-75">
+              <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:80%">80%</div>
+            </div>
+          </div>
+      
+
+          <div class="name my-3">
+            <h1>CSS</h1>
+            <div class="progress w-75">
+              <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:70%">70%</div>
+            </div>
+          </div>
+          
+
+          <div class="name my-3">
+            <h1>JavaScript</h1>
+            <div class="progress w-75">
+              <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:78%">78%</div>
+            </div>
+          </div>
+          
+          <div class="name my-3">
+            <h1>PHP</h1>
+              <div class="progress w-75">
+               <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">40%</div>
+            </div>
+          </div>
+
+
+          <div class="name my-3">
+            <h1>PYTHON</h1>
+              <div class="progress w-75">
+                <div class="progress-bar progress-bar-striped active" role="progressbar"
+              aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:80%">80%</div>
+            </div>  
+         </div> 
+        </div>
+
+        <div class="col-lg-6 col-md-12 col-12 servicediv">
+          <div class="row">
+
+            <div class="col-lg-2 col-2 service-icons">
+              <i class="fa-3x fa fa-desktop" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-10 col-10">
+              <h2> Website Development</h2>
+              <p>We offer most and attractive price for you. hurry up</p>
+            </div>
+          </div>
+
+          <div class="row">
+            
+            <div class="col-lg-2 col-2 service-icons">
+              <i class="fa-3x fa fa-wifi" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-10 col-10">
+              <h2> Digital marketing</h2>
+              <p>We offer most and attractive price for you. hurry up</p>
+            </div>
+          </div>
+
+          <div class="row">
+            
+            <div class="col-lg-2 col-2 service-icons">
+              <i class="fa-3x fa fa-phone" aria-hidden="true"></i>
+            </div>
+            <div class="col-lg-10 col-10">
+              <h2> Support 24/7</h2>
+              <p>We offer most and attractive price for you. hurry up</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+  </div>
+
+</section>
+
+<!--end offer section start-->
+
+
+
+
+
+<!---Happy client start---->
+
+<section class="project-work">
+  <div class="container heading text-center">
+    <p class="text-center font-weight-bold">MORE THAN 2,000 WEBSITES CREATED</p>  
+  </div>
+
+  <div class="container d-flex justify-content-around align-item-center">
+    <div>
+      <h1 class="count">1500</h1>
+      <p>CMS installation</p>
+    </div>
+
+    <div>
+      <h1 class="count">2500</h1>
+      <p>Awards Won</p>
+    </div>
+
+    <div>
+      <h1 class="count">700</h1>
+      <p>Happy clients</p>
+    </div>
+
+    <div>
+      <h1 class="count">500</h1>
+      <p>Working on</p>
+    </div>
+  </div>
+
+</section>
+
+<!---ends happy client----->
+
+
+
+
+
+
+<!---Our Pricing start--------->
+<section class="pricing" id='pricingdiv'>
+  <div class="container heading text-center">
+    <h1 class="text-center font-weight-bold">OUR BEST PRICING</h1>  
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card text-center">
+          <div class="card-header">BASIC</div>
+            <div class="card-body">
+            <li>$<span class="money">20</span>/website</li>
+            <li>Responsive Websites</li>
+            <li>Domain Name Free</li>
+            <li>Mobile Friendly</li>
+            <li>Webmail support</li>
+            <li>Customer Support 24/7</li>
+            </div>
+          <div class="card-footer"><a href="#">Purchase</a></div>
+          </div>
+        </div> 
+      
+
+      <div class="col-lg-4 col-md-4 col-12 second-card">
+        <div class="card text-center">
+          <div class="card-header">STANDARD</div>
+            <div class="card-body">
+              <li>$<span class="money">40</span>/website</li>
+              <li>Responsive Websites</li>
+              <li>Domain Name Free</li>
+              <li>Mobile Friendly</li>
+              <li>Webmail support</li>
+              <li>Customer Support 24/7</li>
+            </div>
+            <div class="card-footer"><a href="#">Purchase</a></div>
+        </div> 
+      </div> 
+
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card text-center">
+          <div class="card-header">UNLIMITED</div>
+            <div class="card-body">
+              <li>$<span class="money">60</span>/website</li>
+              <li>Responsive Websites</li>
+              <li>Domain Name Free/li>
+              <li>Mobile Friendly</li>
+              <li>Webmail support</li>
+              <li>Customer Support 24/7</li>
+            </div>
+            <div class="card-footer"><a href="#">Purchase</a></div>
+        </div> 
+      </div> 
+
+    </div>
+  </div>
+</section>
+<!---Pricing end-------------->
+
+
+
+<!---Customer feedback start---->
+
+<section class="happyclients">
+  <div class="container heading text-center">
+    <h1 class="text-center font-weight-bold">OUR HAPPY CLIENTS</h1>
+    <P class='text-capitalize pt-1'>Our satisfied customer says</P>  
+  </div>
+
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner container">
+    <div class="carousel-item active">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+  
+          </div>          
+        </div>
+
+         <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+
+            
+          </div>
+        </div>
+
+         <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+
+            
+          </div>  
+        </div>
+      </div>
+
+    </div>
+    <div class="carousel-item">
+      
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+  
+          </div>          
+        </div>
+
+         <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+
+            
+          </div>
+        </div>
+
+         <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+
+            
+          </div>  
+        </div>
+      </div>
+
+    </div>
+    <div class="carousel-item">
+      
+        <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+  
+          </div>          
+        </div>
+
+         <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>atul kumar</h1>
+            <h2>Web developer</h2>
+
+            
+          </div>
+        </div>
+
+         <div class="col-lg-4 col-md-4 col-12">
+          <div class="box">
+            <a href="#"><img src="coffee.jpg" class='img-fluidi img-thumbnail'></a>
+
+            <p class="m-4">i got the world class wed site from abc company</p>
+
+            <h1>Atul Kumar</h1>
+            <h2>Web developer</h2>
+
+            
+          </div>  
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+  </div>
+</section>
+
+<!---Customer feedback ends---->
+
+
+<!---Contact us---->
+<section class="contactus">
+  <div class="container heading text-center">
+    <h1 class="text-center font-weight-bold">CONTACT US</h1>
+    <p class="text-center">we are Here to help and answer any question you might have. we look forward to hearing from you.</p> </div>
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-8 col-10 offset-lg-2 offset-md-2 col-1">
+          <form action="/action_page.php">
+
+
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Enter your name" id="username" required="">
+            </div>
+
+
+            <div class="form-group">
+              <input type="email" class="form-control" placeholder="Enter your email" id="email" required="">
+            </div>
+
+            <div class="form-group">
+              <input type="number" class="form-control" placeholder="Enter mobile number" id="mobile" required="">
+              </div>
+
+            <div class="form-group form-check">
+              <label class="form-check-label">
+              <input class="form-check-input" type="checkbox"> Remember me
+              </label>
+            </div>
+
+            <div class="form-group">
+            <label for="comment">Comment:</label>
+            <textarea class="form-control" rows="5" id="comment" placeholder="Enter your Message"></textarea>
+            </div>
+
+            <div class="d-flex justify-content-center form-button">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+</section>
+
+
+
+<!--End contact us----->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfMEttF6AV2DF2fFNE=" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.js" integrity="sha256-wldfwyVJyA71oKe6Sba0fKZkaR6CMwtb0DnWcQs6N1Y=" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+<!--for counting--->
+<script>
+  $('.count').counterUp({
+    delay:10,
+    time:3000
+  })
+</script>
+<!---counting end---->
+
+</body>
+</html>
